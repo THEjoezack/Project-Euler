dd6d3a6d1a0a39f30c15682788a0f2e599d44514
joe <joe@blizzard2.(none)>
2013-05-27 22:16:09 -0400

    Adding solution for 40

---
+++ 040.rb
@@ -0,0 +1,16 @@
+result = 1
+count = 0
+digit = 0
+
+7.times do |i|
+  target = 10**i
+
+  while count < target
+    digit += 1
+    count += Math.log10(digit).to_i + 1
+  end
+
+  result *= digit.to_s.reverse[count - target].to_i
+end
+
+puts result 
