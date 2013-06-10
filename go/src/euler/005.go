5237d41f4a00fc19760648b138afb9cf25e88443
joe <me@joezack.com>
2013-06-10 19:43:30 -0400

    Fixing up the pseudo code...too bad it's a faulty algorithm

--- go/src/euler/005.go
+++ go/src/euler/005.go
@@ -1,29 +1,27 @@
-// 2520 is the smallest number that can be
-// divided by each of the numbers from 1 to 10
+// 2520 is the smallest number that can be divided by each of the numbers from 1 to 10
 // without any remainder.
 
-// What is the smallest number that is evenly
-// divisible by all of the numbers from 1 to 20?
+// What is the smallest number that is evenly divisible by all of the numbers from 1 to 20?
+package main
 
-import (
-	"euler/lib"
-	"fmt"
-	"math"
-)
+import "fmt"
 
-func main (
-	p := lib.NewPrimeGenerator()
-	p.IsPrime?(20) // cheap way of getting the prime factors
-	current := 2520
+func main() {
+	primes := []int{19,17,13,11,7,5,3,2} // primes < 20
+	current := 2520 // has to be > the given for 10
 	for {
-		for i,v in p.Primes.Reverse {
-			if current % v != 0 {
+		found := true
+		for i := 0; i < len(primes); i++ {
+			if current % primes[i] == 0 {
 				continue
 			}
-			return
+			found = false	
+			break
+		}
+		if found {
+			break	
 		}
 		current += 1
 	}
-)
-
-fmt.Println(current)
+	fmt.Println(current)
+}
