98b8d6cda2389eb0bec337651df0871042cf696c
joe <joe@joes-MacBook-Pro.local>
2014-04-20 21:07:24 -0400

    Minor cleanup for #46

--- ruby/046.rb
+++ ruby/046.rb
@@ -5,27 +5,25 @@ doubles = [2]
 results = {}
 
 n = 7
+
 begin
   n += 2
   last_index = primer.stack.count
   primer.fill_to n
 
   (last_index..primer.stack.count - 1).each do |prime_index|
-  	last_prime = primer.stack[prime_index]
     doubles.each do |double|
-      results[last_prime + double] = 0
+      results[primer.stack[prime_index] + double] = 0
     end
   end
-
-  next if primer.is_prime? n
 
   while doubles.last < n
-  	double = 2 * doubles.count * doubles.count
-    doubles << double
+    doubles << 2 * doubles.count * doubles.count
     primer.stack.each do |prime|
-      results[prime + double] = 0
+      results[prime + doubles.last] = 0
     end
   end
+
 end while results.has_key?(n) || primer.is_prime?(n)
 
 puts n
