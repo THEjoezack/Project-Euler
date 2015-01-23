26de4bb8a7819c27d02651c12bf3561da28196b0
THEjoezack <me@joezack.com>
2015-01-23 15:26:00 -0500

    Adding 1.11 solution

--- scheme/001.scm
+++ scheme/001.scm
@@ -1,4 +1,4 @@
-((define (problem sum i max)
+(define (problem sum i max)
   (define (divides divisor) (= 0 (modulo i divisor)))
   (define (get-sum) (if (or (divides 5) (divides 3)) (+ i sum) sum))
   (if (= i max) sum (problem (get-sum) (+ i 1) max)))
