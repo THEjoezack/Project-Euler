771514b5904953c716673e8ad0874b1939cddd86
THEjoezack <me@joezack.com>
2015-01-22 19:34:38 -0500

    Slightly improved scheme 001

--- scheme/001.scm
+++ scheme/001.scm
@@ -1,8 +1,11 @@
 (define (problem sum i count)
   (define (divides divisor) (= 0 (modulo i divisor)))
-  (cond
-    ((= 0 count) sum)
-    ((or (divides 5) (divides 3)) (problem (+ sum i) (+ i 1) (- count 1)))
-    (else (problem sum (+ i 1) (- count 1)))))
+  (define (divisible) (or (divides 5) (divides 3)))
+  (if (= 0 count)
+    sum
+    (problem
+      (if (divisible) (+ i sum) sum)
+      (+ i 1)
+      (- count 1))))
 
 (problem 0 1 999)
