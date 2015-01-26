c5ea089511dfb2e579851a0fa30e920f44f87567
THEjoezack <me@joezack.com>
2015-01-26 09:20:14 -0500

    Adding swift project, problem #1

---
+++ swift/euler/euler/P001.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+class P001 {
+    func run() -> String {
+        let filtered = filter(1...999, { $0 % 3 == 0 || $0 % 5 == 0 })
+        let sum = filtered.reduce(0, +) // chaining these together results in a compile error?
+
+        return String(sum);
+    }
+}
---
+++ swift/euler/euler/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  euler
+//
+//  Created by joe on 1/25/15.
+//  Copyright (c) 2015 joe. All rights reserved.
+//
+
+import Foundation
+
+println(P001().run());
